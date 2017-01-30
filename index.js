a37633b3f97a70fbbddda59044625b3d3f8dd58a
Arvind6353 <arvindan92@gmail.com>
2017-01-30 23:46:16 +0530

    bot

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {  
- 		   if (!kittenMessage(event.sender.id, event.message.text)) {
+ 		   if (!picMessage(event.sender.id, event.message.text)) {
         		sendMessage(event.sender.id, {text: event.message.text +" :) "});
     		}
 		}
@@ -56,43 +56,68 @@ function sendMessage(recipientId, message) {
 
 
 // send rich message with kitten
-function kittenMessage(recipientId, text) {
+function picMessage(recipientId, text) {
 
     text = text || "";
     var values = text.split(' ');
+    var flag=false;
+
+    if(values[0].toLowerCase()=='sadness'){
+
+    	var imageUrl = "https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjUjuvJuurRAhUIsI8KHdYXCXUQjRwIBw&url=http%3A%2F%2Fdisney.wikia.com%2Fwiki%2FSadness&psig=AFQjCNFX3p0BuYVOaXvtheynPG8aGCdfWg&ust=1485885928006565";
+    	flag=true;
+    }
+
+
+    if(values[0].toLowerCase()=='pizza'){
+
+    	var imageUrl = "https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwillaisvOrRAhVKvI8KHUXTARYQjRwIBw&url=http%3A%2F%2Fmartinionheels.com%2Ffive-people-who-would-simply-love-quattro-formaggi-burst-pizza-by-dominos%2F&psig=AFQjCNHBGyvqwCBJVi810Tuu-sWNj-xf2w&ust=1485886390300294";
+    	flag=true;
+    }
+
+
+
+    if(values[0].toLowerCase()=='chinchan' || values[0].toLowerCase()=='shinchan' || values[0].toLowerCase()=='sinchan'){
+
+    	var imageUrl = "https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwigtLa4vOrRAhVBu48KHZIaDa4QjRwIBw&url=https%3A%2F%2Fwww.walldevil.com%2F216767-shin-chan-wallpaper.html&psig=AFQjCNGd3mGHSJ_prZJ4KrOEAENGWfIung&ust=1485886424342163";
+    	flag=true;
+    }
 
     if (values.length === 3 && values[0] === 'kitten') {
         if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-
-            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-
-            message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": imageUrl ,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": imageUrl,
-                                "title": "Show kitten"
-                                }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
-                    }
-                }
-            };
-
-            sendMessage(recipientId, message);
-
-            return true;
+		    var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+			flag=true;
         }
     }
+
+
+	if(flag){
+	     message = {
+	                "attachment": {
+	                    "type": "template",
+	                    "payload": {
+	                        "template_type": "generic",
+	                        "elements": [{
+	                            "title": values[0],
+	                            "subtitle": "How's it ;)",
+	                            "image_url": imageUrl ,
+	                            "buttons": [{
+	                                "type": "web_url",
+	                                "url": imageUrl,
+	                                "title": "View Full Image"
+	                                }/*, {
+	                                "type": "postback",
+	                                "title": "I like this",
+	                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+	                            }*/]
+	                        }]
+	                    }
+	                }
+	            };
+	            
+	      sendMessage(recipientId, message);
+	}
+
+            return flag;
 
 }
