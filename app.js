2f00ed1d312a863eb4125f4e581399f2e8415f78
Arvind6353 <arvindan92@gmail.com>
2017-01-31 18:01:50 +0530

    bot

--- app.js
+++ app.js
@@ -539,7 +539,38 @@ var noImageUrl = 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTMEkUULO
 	        });
 				
 		}
-		sendRequest(sen, JSON.stringify(messageData)); 
+
+		 messageData={
+		    "attachment":{
+		      "type":"template",
+		      "payload":{
+		        "template_type":"generic",
+		        "elements":[
+		           {
+		            "title":"Welcome to Peter\'s Hats",
+		            "image_url":"https://petersfancybrownhats.com/company_image.png",
+		            "subtitle":"We\'ve got the right hat for everyone.",
+		            "default_action": {
+		              "type": "web_url",
+		              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+		              "messenger_extensions": true,
+		              "webview_height_ratio": "tall",
+		              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+		            },
+		            "buttons":[
+		              {
+		                "type":"web_url",
+		                "url":"https://petersfancybrownhats.com",
+		                "title":"View Website"
+		              }           
+		            ]      
+		          }
+		        ]
+		      }
+		    }
+		  }
+
+		sendRequest(sender, JSON.stringify(messageData)); 
     });
 
 }
