28357a2c0eb2efebf77f5fd10ba4045d43366c4a
Arvind6353 <arvindan92@gmail.com>
2017-01-31 18:18:01 +0530

    bot

--- app.js
+++ app.js
@@ -540,7 +540,7 @@ var noImageUrl = 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTMEkUULO
 				
 		}
 
-		console.log("data ----"+messageData)
+		console.log("data ----"+JSON.stringify(messageData));
 		sendRequest(sender, messageData); 
     });
 
